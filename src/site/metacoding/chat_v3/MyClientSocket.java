2ef5ab94277167326e6f9a11ba22e315f59457f0
해윤 <whgodbs32191@gmail.com>
2022-02-16 17:54:55 +0900

    채팅 - 프로토콜 구현

--- src/site/metacoding/chat_v3/MyClientSocket.java
+++ src/site/metacoding/chat_v3/MyClientSocket.java
@@ -1,4 +1,4 @@
-package site.metacoding.chat_v2;
+package site.metacoding.chat_v3;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -13,12 +13,13 @@ public class MyClientSocket {
     // 채팅을 작성하는데 필요한 스레드
     BufferedWriter writer;
     Scanner sc;
-
+    String username;
     // 서버에서 받아오는 메세지를 읽어오는 스레드
     BufferedReader reader;
 
     public MyClientSocket() {
-
+        // ALL:와 할거 많다
+        // CHAT:
         try {
             socket = new Socket("192.168.0.132", 2000);
             // 192.168.0.132
@@ -27,8 +28,13 @@ public class MyClientSocket {
             writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             읽기전담스레드 t = new 읽기전담스레드();
-            new Thread(t).start();
 
+            new Thread(t).start();
+            System.out.println("아이디를 입력하세요");
+            username = sc.nextLine();
+            writer.write(username + "\n");
+            writer.flush();
+            System.out.println("환영합니다 " + username + "님 " + username + "이 서버를 전송되었습니다.");
             while (true) {
                 String keyboardInputData = sc.nextLine();
                 writer.write(keyboardInputData + "\n"); // 버퍼에 담는것
@@ -48,7 +54,7 @@ public class MyClientSocket {
             try {
                 while (true) {
                     String inputData = reader.readLine();
-                    System.out.println(" 받은 메세지  : " + inputData);
+                    System.out.println(inputData);
                 }
             } catch (Exception e) {
                 // TODO: handle exception
--- src/site/metacoding/chat_v3/MyServerSocket.java
+++ src/site/metacoding/chat_v3/MyServerSocket.java
@@ -1,4 +1,4 @@
-package site.metacoding.chat_v2;
+package site.metacoding.chat_v3;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -9,6 +9,13 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+
+//프로토콜
+//1.최초 메세지는 username으로 체킹
+//2.구분자 : 
+
+//ALL:메세지
+//CHAT:아이디:메세지
 
 public class MyServerSocket {
 
@@ -30,6 +37,7 @@ public class MyServerSocket {
                 고객리스트.add(t);
                 System.out.println("고객리스트 크기 " + 고객리스트.size());
                 new Thread(t).start();
+
             }
 
         } catch (Exception e) {
@@ -44,6 +52,7 @@ public class MyServerSocket {
         BufferedReader reader;
         BufferedWriter writer;
         boolean isLogin = true;
+        String username;
 
         public 고객전담스레드(Socket socket) {
             this.socket = socket;
@@ -62,17 +71,70 @@ public class MyServerSocket {
 
         }
 
+        // ALL : 채팅 내역
+        public void chatPublic(String msg) {
+            try {
+                System.out.println("전체채팅");
+                for (고객전담스레드 t : 고객리스트) { // 왼쪽 : 컬렉션 타입 ,오른쪽 : 컬렉션
+                    if (t != this) {
+                        t.writer.write("[전체]" + username + ":" + msg + "\n");
+                        t.writer.flush();
+                    }
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+
+        }
+
+        // CHAT : 아이디 : 채팅내역
+        public void chatPrivate(String username, String msg) {
+            try {
+                System.out.println("귓속말");
+                for (고객전담스레드 t : 고객리스트) { // 왼쪽 : 컬렉션 타입 ,오른쪽 : 컬렉션
+                    if (t.username.equals(username)) {
+                        t.writer.write("[귓속말]" + username + ":" + msg + "\n");
+                        t.writer.flush();
+                    }
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+
+        // 메세지 파싱 =프로토콜
+        public void jwp(String inputData) {
+            // 1.프로토콜 분리
+            String[] token = inputData.split(":");
+            String protocol = token[0];
+            if (protocol.equals("ALL")) {
+                String msg = token[1];
+                chatPublic(msg);
+            } else if (protocol.equals("CHAT")) {
+                String username = token[1];
+                String msg = token[2];
+                chatPrivate(username, msg);
+            }
+
+        }
+
         @Override
         public void run() {
+            // 최초 메세지는 username 이다.
+            try {
+                username = reader.readLine();
+                isLogin = true;
+            } catch (Exception e) {
+                // TODO: handle exception
+                isLogin = false;
+                System.out.println("username 을 받지 못했습니다.");
+            }
+
             while (isLogin) {
                 try {
                     String inputData = reader.readLine();
                     System.out.println("from  클라이언트 : " + inputData);
-
-                    for (고객전담스레드 t : 고객리스트) { // 왼쪽 : 컬렉션 타입 ,오른쪽 : 컬렉션
-                        t.writer.write(inputData + "\n");
-                        t.writer.flush();
-                    }
+                    jwp(inputData);
                     // 메세지를 받았으니까 List < 고객전담스레드 > 고객리스트 <== 여기에 담긴
                     // 모든 클라이언트에게 메세지를 전송 for each문 돌려서!
                 } catch (Exception e) {
